899d667debac61853b697065a945cf5400fa30f4
trasku <rasku97@gmail.com>
2025-01-25 14:08:39 +0200

    unicafe 1.7 luotu

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -6,25 +6,50 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const Average = ({ feedbacks }) => {
+  const amountOfFeedbacks = feedbacks.length
+  const average = amountOfFeedbacks
+    ? feedbacks.reduce((sum, value) => sum + value, 0) / amountOfFeedbacks
+    : 0
+  return (
+    <p>average {average}</p>
+  )
+}
+
+const PositiveRatio = ({ good, feedbacks }) => {
+  const amountOfFeedbacks = feedbacks.length
+  const positiveRatio = good
+    ? good / amountOfFeedbacks
+    : 0
+  
+  return (
+    <p>positive {positiveRatio*100} %</p>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [feedbacks, setFeedback] = useState([])
 
   const goodFB = () => {
     console.log('good feedback given')
     setGood(good + 1)
+    setFeedback(feedbacks.concat(1))
   }
 
   const neutralFB = () => {
     console.log('neutral feedback given')
     setNeutral(neutral + 1)
+    setFeedback(feedbacks.concat(0))
   }
 
   const badFB = () => {
     console.log('bad feedback given')
     setBad(bad + 1)
+    setFeedback(feedbacks.concat(-1))
   }
 
   return (
@@ -37,6 +62,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {feedbacks.length}</p>
+      <Average feedbacks={feedbacks} />
+      <PositiveRatio good={good} feedbacks={feedbacks} />
     </div>
   )
 }
